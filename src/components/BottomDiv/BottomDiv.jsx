2b8b19f055367e78c7faa031b3df845b94f636ba
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-11-28 00:00:14 +0530

    mobile changes

--- src/components/BottomDiv/BottomDiv.jsx
+++ src/components/BottomDiv/BottomDiv.jsx
@@ -1,22 +1,24 @@
-import styles from './BottomDiv.module.scss';
+import styles from "./BottomDiv.module.scss";
 import attackImage from "/svgs/bottom-left.svg";
 import oasisLogo from "/imgs/oasis-logo.png";
 import apogeeLogo from "/imgs/apogee-logo.png";
 import bosmLogo from "/imgs/bosm-logo.png";
 // import soundImage from "/imgs/middle-right-3.png";
-import COCButton from '../cocButton/COCButton';
-import { useState } from 'react';
-import { IoVolumeHigh, IoVolumeMute } from 'react-icons/io5';
-import { FaStar } from 'react-icons/fa';
+import COCButton from "../cocButton/COCButton";
+import { useState, useRef } from "react";
+import { IoVolumeHigh, IoVolumeMute } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
 import mapIcon from "/imgs/coc-map-icon.png";
+import bgMusic from "/audio/bg-music.mp3";
 
 export default function BottomDiv() {
+  const [musicPlaying, setMusicPlaying] = useState(false); // later recieved via state
+  const audioRef = useRef(new Audio(bgMusic));
 
-    const [musicPlaying, setMusicPlaying] = useState(false); // later recieved via state
-
-    return (
-        <div className={styles.bottomDiv}>
-            {/* <COCButton className={styles.attackButton} color="#C46A15">
+  return (
+    <div className={styles.bottomDiv}>
+      <audio src={bgMusic} loop ref={audioRef}></audio>
+      {/* <COCButton className={styles.attackButton} color="#C46A15">
                 <div className={styles.starsContainer}>
                     {
                         Array(5).fill(null).map((_, i) => <FaStar className={styles.star} stroke="#f4973e" strokeWidth={36} />)
@@ -24,37 +26,44 @@ export default function BottomDiv() {
                 </div>
                 <img className={styles.mapImg} src={mapIcon} />
             </COCButton> */}
-            <div className={styles.festContainer}>
-                <COCButton className={styles.festLogoWrapper} color="#C46A15">
-                    <img
-                        src={bosmLogo}
-                        alt="bosm-logo"
-                        className={styles.festLogo}
-                    />
-                </COCButton>
-                <COCButton className={styles.festLogoWrapper} color="#C46A15">
-                    <img
-                        src={oasisLogo}
-                        alt="oasis-logo"
-                        className={styles.festLogo}
-                    />
-                </COCButton>
-                <COCButton className={styles.festLogoWrapper} color="#C46A15">
-                    <img
-                        src={apogeeLogo}
-                        alt="apogee-logo"
-                        className={styles.festLogo}
-                    />
-                </COCButton>
-            </div>
-            {/* <img src={soundImage} className={styles.soundButton} /> */}
-            <COCButton className={styles.soundButton} onClick={() => setMusicPlaying(prev => !prev)} color="#C46A15">
-                {
-                    musicPlaying ?
-                        <IoVolumeHigh className={styles.volumeIcon} strokeWidth={20} stroke="#000" />
-                        : <IoVolumeMute className={styles.volumeIcon} strokeWidth={20} stroke="#000" />
-                }
-            </COCButton>
-        </div>
-    )
+      <div className={styles.festContainer}>
+        <COCButton className={styles.festLogoWrapper} color="#C46A15">
+          <img src={bosmLogo} alt="bosm-logo" className={styles.festLogo} />
+        </COCButton>
+        <COCButton className={styles.festLogoWrapper} color="#C46A15">
+          <img src={oasisLogo} alt="oasis-logo" className={styles.festLogo} />
+        </COCButton>
+        <COCButton className={styles.festLogoWrapper} color="#C46A15">
+          <img src={apogeeLogo} alt="apogee-logo" className={styles.festLogo} />
+        </COCButton>
+      </div>
+      {/* <img src={soundImage} className={styles.soundButton} /> */}
+      <COCButton
+        className={styles.soundButton}
+        onClick={() => {
+          setMusicPlaying((prev) => !prev);
+          if (musicPlaying) {
+            audioRef.current.pause();
+          } else {
+            audioRef.current.play();
+          }
+        }}
+        color="#C46A15"
+      >
+        {musicPlaying ? (
+          <IoVolumeHigh
+            className={styles.volumeIcon}
+            strokeWidth={20}
+            stroke="#000"
+          />
+        ) : (
+          <IoVolumeMute
+            className={styles.volumeIcon}
+            strokeWidth={20}
+            stroke="#000"
+          />
+        )}
+      </COCButton>
+    </div>
+  );
 }
